acadd92e33dec51f510a6ad1140dd01e1ab69557
Kaye <kayeduenlagahid@gmail.com>
2025-05-25 20:26:24 +0800

    updated the all post analysis

--- nlp_2/views.py
+++ nlp_2/views.py
@@ -1,26 +1,33 @@
 @api_view(["GET"])
 def all_post_analysis(request):
+    from transformers import pipeline
+    from nlp.matching import topic_classifier
+    from data.models import Post
+
     sentiment_pipeline = pipeline(
-        "sentiment-analysis", 
-        model="nlptown/bert-base-multilingual-uncased-sentiment"
+        "sentiment-analysis",
+        model="distilbert-base-uncased-finetuned-sst-2-english"
     )
 
     result = []
 
     for post in Post.objects.all():
         sentiment_result = sentiment_pipeline(post.content)[0]
-        sentiment = sentiment_result["label"]
+        sentiment = sentiment_result["label"].lower()  # e.g. 'positive'
+        score = round(sentiment_result["score"], 3)
         topics = topic_classifier(post.id)
 
-      
+        #save sa datbase
         post.sentiment = sentiment
+        post.sentiment_score = score
         post.topics = topics
         post.save()
 
         result.append([
             post.id,
             topics,
-            sentiment
+            sentiment,
+            score
         ])
 
-    return Response(result) 
+    return Response(result)
