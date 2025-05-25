f1739be03ec51b0cd6fe0bb68312e67bbbba6771
Kaye <kayeduenlagahid@gmail.com>
2025-05-25 19:51:56 +0800

    Add view to analyze every post in table format

--- nlp_2/urls.py
+++ nlp_2/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import analyze_post
+from .views import analyze_post, all_post_analysis
 
 urlpatterns = [
     path("analyze/", analyze_post),
+    path("all-posts/", all_post_analysis),  
 ]
